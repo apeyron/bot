1a60b07d411cb8dc0cdb45d32196ac4c0e315d3f
sokol <sistema@newmail.ru>
2018-03-11 00:02:48 +0300

    перезалив3

--- bbot.py
+++ bbot.py
@@ -1,35 +1,45 @@
 # -*- coding: utf-8 -*-   
-import telebot
-import random
+import logging
 import os
-import request
-from telebot import types
 
-hi = ["Hello", "Hi", "hi", "Привет"]
-TOKEN = '210719426:AAFGyWjfVT5eLUC1DnPmTS1iJkuL-FpOeP4'
-
-bbot = telebot.TeleBot(TOKEN)
-
-@bbot.message_handler(commands=['start'])
-def keyb(message):
-    k = types.ReplyKeyboardMarkup()
-    k.row('1', '2', '3')
-    bbot.send_message(message.chat.id, "Выберите пункт", reply_markup=k)
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
-""""@bbot.message_handler(commands=["start"])
-def start(message):
-    bbot.send_message(message.chat.id, "Ты с нами " + message.chat.first_name)
-"""
-
-@bbot.message_handler(content_types=["text"])
-def main(message):
-    if message.text in hi:
-        bbot.send_message(message.chat.id, random.choice(hi))
-    else:
-        #message.text != "Hi":
-        bbot.send_message(message.chat.id, "Не понятно...")
+def start(bot, update):
+    update.effective_message.reply_text("Hi!")
 
 
-bot.delete_webhook()
-bot.polling(none_stop=True)
+def echo(bot, update):
+    update.effective_message.reply_text(update.effective_message.text)
+
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+if __name__ == "__main__":
+    # Set these variable to the appropriate values
+    TOKEN = "210719426:AAFGyWjfVT5eLUC1DnPmTS1iJkuL-FpOeP4"
+    NAME = "botbankrot"
+
+    # Port is given by Heroku
+    PORT = os.environ.get('PORT')
+
+    # Enable logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    # Set up the Updater
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    # Add handlers
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(error)
+
+    # Start the webhook
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+updater.idle()
