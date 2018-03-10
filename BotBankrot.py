92d0904b935520e36ec68e0dba3070838dafdc7e
sokol <sistema@newmail.ru>
2018-03-10 18:28:55 +0300

    перезалив3

--- BotBankrot.py
+++ BotBankrot.py
@@ -31,5 +31,4 @@ def main(message):
         #message.text != "Hi":
         bbot.send_message(message.chat.id, "Не понятно...")
 
-if __name__ == "__main__":
-    bbot.polling(none_stop=True)
+bbot.polling(none_stop=True, interval=0)
