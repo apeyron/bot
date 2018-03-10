943472cffd282ec38d7dc37bfae3a9db67fa00e4
sokol <sistema@newmail.ru>
2018-03-10 20:06:16 +0300

    перезалив4

--- BotBankrot.py
+++ BotBankrot.py
@@ -16,8 +16,6 @@ def keyb(message):
     k.row('1', '2', '3')
     bbot.send_message(message.chat.id, "Выберите пункт", reply_markup=k)
 
-@bbot.message_handler(content_types=)
-
 
 """"@bbot.message_handler(commands=["start"])
 def start(message):
@@ -32,25 +30,6 @@ def main(message):
         #message.text != "Hi":
         bbot.send_message(message.chat.id, "Не понятно...")
 
-		
-# Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
-if "HEROKU" in list(os.environ.keys()):
-    logger = telebot.logger
-    telebot.logger.setLevel(logging.INFO)
-
-    server = Flask(__name__)
-    @server.route("/bot", methods=['POST'])
-    def getMessage():
-        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
-    @server.route("/")
-    def webhook():
-        bot.remove_webhook()
-        bot.set_webhook(url="https://botbankrot.herokuapp.com/bot") # этот url нужно заменить на url вашего Хероку приложения
-        return "?", 200
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
-else:
-    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.  
-    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
-    bot.remove_webhook()
-    bot.polling(none_stop=True)
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 80))
+    bbot.run(debug=False, port=port, host='0.0.0.0')
