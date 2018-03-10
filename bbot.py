79b0c9bde0a3c0392d68003e1d08a5978e3c9dbb
sokol <sistema@newmail.ru>
2018-03-11 00:09:33 +0300

    перезалив3

--- bbot.py
+++ bbot.py
@@ -1,45 +1,30 @@
 # -*- coding: utf-8 -*-   
-import logging
-import os
+import telebot
+from telebot import types
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+hi = ["Hello", "Hi", "hi", "Привет"]
+TOKEN = '210719426:AAFGyWjfVT5eLUC1DnPmTS1iJkuL-FpOeP4'
 
+bbot = telebot.TeleBot(TOKEN)
 
-def start(bot, update):
-    update.effective_message.reply_text("Hi!")
-
-
-def echo(bot, update):
-    update.effective_message.reply_text(update.effective_message.text)
-
-def error(bot, update, error):
-    logger.warning('Update "%s" caused error "%s"', update, error)
+@bbot.message_handler(commands=['start'])
+def keyb(message):
+    k = types.ReplyKeyboardMarkup()
+    k.row('1', '2', '3')
+    bbot.send_message(message.chat.id, "Выберите пункт", reply_markup=k)
 
 
-if __name__ == "__main__":
-    # Set these variable to the appropriate values
-    TOKEN = "210719426:AAFGyWjfVT5eLUC1DnPmTS1iJkuL-FpOeP4"
-    NAME = "botbankrot"
+""""@bbot.message_handler(commands=["start"])
+def start(message):
+    bbot.send_message(message.chat.id, "Ты с нами " + message.chat.first_name)
+"""
 
-    # Port is given by Heroku
-    PORT = os.environ.get('PORT')
+@bbot.message_handler(content_types=["text"])
+def main(message):
+    if message.text in hi:
+        bbot.send_message(message.chat.id, random.choice(hi))
+    else:
+        #message.text != "Hi":
+        bbot.send_message(message.chat.id, "Не понятно...")
 
-    # Enable logging
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
-    logger = logging.getLogger(__name__)
-
-    # Set up the Updater
-    updater = Updater(TOKEN)
-    dp = updater.dispatcher
-    # Add handlers
-    dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(MessageHandler(Filters.text, echo))
-    dp.add_handler(error)
-
-    # Start the webhook
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
-updater.idle()
+bbot.polling(none_stop=True)
